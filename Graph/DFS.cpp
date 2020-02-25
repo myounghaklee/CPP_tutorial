02d359b2d4a7d95a306e063345e398f86f9da010
이명학 <leemh9207@ajou.ac.kr>
2020-02-25 14:42:44 +0900

    dfs 끝

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -8,9 +8,21 @@ void dfs(int x){
     
     check[x] = true;
     printf("%d",x);
-    for( int i=0 ; i<=n;i++){
+    for( int i=0; i<=n; i++){//n은 정점의 갯수
         if(a[x][i]==1 && check[i]==false){
             dfs(i);
+        }
+    }
+}
+
+//인접 리스트를 이용하여 구현 
+void dfs(int x){
+    check[x]=true;
+    printf("%d",x);
+    for( int i=0; i<a[x].size(); i++){
+        int y = a[y][i];
+        if(check[y] == false){
+            dfs(y);
         }
     }
 }
