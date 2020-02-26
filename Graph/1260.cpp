1d2ec88b6221a016e197bd67a0d81ea7f3333d09
이명학 <leemh9207@ajou.ac.kr>
2020-02-26 23:02:07 +0900

    1260진행

---
+++ Graph/1260.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+vector<int> map[1001];
+bool check[1001];
+unsigned int N, M ,V;
+
+void dfs(int node);
+
+
+int main(){
+    scanf("%d %d %d", &N,v&M, &V);
+    for(int i =0; i<M; i++){
+        int u, v; 
+        scanf("%d %d",&u, &v );
+
+
+    }
+    
+
+    return 0;
+}
