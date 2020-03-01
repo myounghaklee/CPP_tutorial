a728168754e1af6f00f168460b135d10828583e7
이명학 <leemh9207@ajou.ac.kr>
2020-03-01 23:44:56 +0900

    2667코드 추가

---
+++ Graph/2667.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<queue>
+#include<algorithm>
+
+int main (){
+    int count=0;
+    for(int i=0;i<n;i++){
+        for(int j =0;j<n;j++){
+            if(a[i][j]==1;&&d[i][j]==0){
+                bfs(i,j,++count);
+            }
+        }
+    }
+}
