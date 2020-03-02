028ad0abf818ce2a9fa5033806312f427e7ba4df
이명학 <leemh9207@ajou.ac.kr>
2020-03-02 19:39:44 +0900

    bfs 작성중

--- Graph/2667.cpp
+++ Graph/2667.cpp
@@ -2,12 +2,17 @@
 #include<queue>
 #include<algorithm>
 
+void bfs(int x, int y , int cnt){
+    queue<pair<int,int,>> q; 
+    
+}
+
 int main (){
     int count=0;
     for(int i=0;i<n;i++){
         for(int j =0;j<n;j++){
             if(a[i][j]==1;&&d[i][j]==0){
-                bfs(i,j,++count);
+                bfs(i,j,++count); 
             }
         }
     }
