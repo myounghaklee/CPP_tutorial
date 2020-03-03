e6ee6d2edb5bcbc26060b1591044d0059b63abc3
이명학 <leemh9207@ajou.ac.kr>
2020-03-03 17:59:07 +0900

    2667

--- Graph/2667.cpp
+++ Graph/2667.cpp
@@ -11,7 +11,16 @@ void bfs(int x, int y , int cnt){
         y= q.front().second;
         q.pop;
         for(int k=0; k<4;k++){
-            
+            int nx = x+dx[k];
+            int ny = y+dy[k];
+            if(0 <= nx && nx < n && 0 <= ny && ny<n){
+                if(a[nx][ny] == 1 &7 d[nx][ny] ==0){
+                    q.push(make_pair(nx,ny));
+                    d[nx][ny] = cnt;
+                }
+
+            }
+
         }
     }
 }
@@ -20,7 +29,7 @@ int main (){
     int count=0;
     for(int i=0;i<n;i++){
         for(int j =0;j<n;j++){
-            if(a[i][j]==1;&&d[i][j]==0){
+            if(a[i][j]==1 && d[i][j]==0){
                 bfs(i,j,++count); 
             }
         }
