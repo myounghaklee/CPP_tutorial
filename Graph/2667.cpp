34c13c7f1ac58fabb4d369f8ff3500defd47776f
이명학 <leemh9207@ajou.ac.kr>
2020-03-05 17:39:31 +0900

    코드 주석추가

--- Graph/2667.cpp
+++ Graph/2667.cpp
@@ -2,6 +2,16 @@
 #include<queue>
 #include<algorithm>
 
+/*
+(x,y)를 방문하는 bfs 
+
+*/
+//현재 정점에서 동서남북으로 이동하기위한 x,y좌표 배열 
+int dx[4]= {1,-1,0,0};
+int dy[4]= {0,0,1,-1};
+
+// nx,ny = 현재정점에서 다음정점으로 이동하기 위함
+
 void bfs(int x, int y , int cnt){
     queue<pair<int,int,>> q; 
     q.push(make_pair(x,y));
@@ -29,7 +39,7 @@ int main (){
     int count=0;
     for(int i=0;i<n;i++){
         for(int j =0;j<n;j++){
-            if(a[i][j]==1 && d[i][j]==0){
+            if(a[i][j]==1 && d[i][j]==0){ //집이 있는데 아직 방분하지 않은경우에 count증가 시킴
                 bfs(i,j,++count); 
             }
         }
