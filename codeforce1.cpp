15e0b733fd4f691edede706077a762d4df69c605
이명학 <leemh9207@ajou.ac.kr>
2020-03-27 00:04:41 +0900

    코드포스

--- codeforce1.cpp
+++ codeforce1.cpp
@@ -1,2 +1,26 @@
 #include <stdio.h>
-#include 
+using namespace std;
+
+void minMove(short n,int a, int b ){
+    unsigned int result = 0;
+    a++;
+    for(int i =0; i<n; i++){
+    if (a%b == 0){
+        printf("%d\n",result);
+    }
+    result++;
+    }
+}
+int main (){
+    unsigned short t;
+    unsigned int a, b;
+    
+    scanf("%hd", &t);
+
+    for(int i=0; i<t; i++){
+        scanf("%d %d", &a ,&b);
+        minMove(t,a,b);
+        
+    }
+    return 0;
+}
