db162ff18acdf85f9bc5dec2fa55cc28bd1f9673
이명학 <leemh9207@ajou.ac.kr>
2020-03-30 00:17:26 +0900

    코드포스

--- codeforce1328.cpp
+++ codeforce1328.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main(){
     int t,n,k;
     scanf("%d",&t);
-    for
+    for(int i =0; i<t;i++){
+        string s(n,'a');
+        for( int j =n-2;j>=0;j--){
+            if(k<=(n-j-1)){
+            s[i]='b';
+            s[n-k]= 'b';
+            printf("%s\n", s);
+            break;
+            }
+            k -=(n-j-1);
+        }
+    }
+    return 0;
 }
