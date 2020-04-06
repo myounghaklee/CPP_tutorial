faee69c60d63537a359487c498edb80edd28ea02
이명학 <leemh9207@ajou.ac.kr>
2020-04-06 23:57:43 +0900

    코드포스

---
+++ codeforce/631_A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespce std;
+
+void solution(){
+    int d, m;
+    cin>>d , m;
+    long long answer =1;
+}
+
+int main (){
+    int t;
+    cin>>t;
+    while(t--){
+        solution();
+    }
+
+    return 0;
+}
