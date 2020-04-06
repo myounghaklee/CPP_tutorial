f251790953504869f4f5421e9762603106273ae7
이명학 <leemh9207@ajou.ac.kr>
2020-04-07 00:14:15 +0900

    코드포스

--- codeforce/631_A.cpp
+++ codeforce/631_A.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
-using namespce std;
+using namespace std;
 
 void solution(){
     int d, m;
-    cin>>d , m;
-    long long answer =1;
+    cin>>d >>m;
+    long long result =1;
+    for( int i =0 ; i<30; i++){
+        if(d<(1<<i)){
+            break;
+        }
+        result = result * (min((1<<(i+1))-1,d)-(1<<i) + 2)%m;
+    }
+    result--;
+    if(result <0){
+        result +=m;
+        cout << result << endl;
+    }
 }
 
 int main (){
