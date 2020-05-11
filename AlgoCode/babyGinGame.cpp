aa006767e4ee29304857abbc7bab45625af9b619
이명학 <leemh9207@ajou.ac.kr>
2020-05-12 01:13:20 +0900

    babygin done

--- AlgoCode/babyGinGame.cpp
+++ AlgoCode/babyGinGame.cpp
@@ -2,12 +2,46 @@
 
 int main()
 {
+    int testcase;
+    int card[6] = {
+        0,
+    };
+    int input;
+    int run, triple;
 
-    int card[6];
-    for (int i = 0; i < 6; i++)
+    scanf("%d", &testcase);
+    for (int i = 0; i < testcase; i++)
     {
-        scanf("%d", &card[i]);
+        scanf("%d", &input);
+    }
+    for (int i = 0; i < 6; i++)
+    { //각 자리수 counting
+        card[(input % 10)]++;
+        input /= 10;
     }
 
+    for (int j = 0; j < 10; j++)
+    {
+        if (card[j] > 2)
+        {
+            card[j] = card[j] - 3;
+            triple += 1;
+            j--;
+        }
+        if (card[j] > 0 && card[j + 1] > 0 && card[j + 2] > 0)
+        {
+            card[j] -= 1;
+            card[j + 1] -= 1;
+            card[j + 2] -= 1;
+            run += 1;
+            j--;
+        }
+    }
+    if ((run + triple) == 2)
+        printf("baby gin");
+    else
+    {
+        printf("lose");
+    }
     return 0;
 }
