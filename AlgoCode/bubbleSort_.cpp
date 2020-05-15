6d6fcf7ca79b604b063e55e8a2a7aeca33505fa0
이명학 <leemh9207@ajou.ac.kr>
2020-05-15 21:23:59 +0900

    선책정렬 중간 커밋

---
+++ AlgoCode/bubbleSort_.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define swap(a,b) {int t; t=a; a=b;b=t;
+
+void SelectionSort(char*a, int size){
+
+    int minimum;
+    int i , j;
+    for(i=0;i<size-1;i++){
+        minimum =i;
+        for(j=i+1;j<size;j++){
+            if(a[minimum]>a[j]){
+                minimum=j;
+                
+            }
+        }
+    }
+}
+
+int array[][];
+
+
+int main(){
+
+
+    return 0;
+}
