96526fcd57f56a98b74b10415b78d4befe70be7d
이명학 <leemh9207@ajou.ac.kr>
2020-06-06 23:20:33 +0900

    진행중

---
+++ BaekJoon/1193.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    int temp =0;
+    int result=0;
+    scanf("%d", &x);
+    for(int i =1 ; i<x; i++){
+        if(i>=x){
+            result =i;
+            break;
+        }
+        temp = x-i;
+        
+    }
+    printf("%d/%d", )
+
+
+    return 0;
+}
