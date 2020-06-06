da25f67d0ba9db24d0dcc6845ebf36c3fdd9b696
이명학 <leemh9207@ajou.ac.kr>
2020-06-07 00:58:38 +0900

    1193중간 커밋

--- BaekJoon/1193.cpp
+++ BaekJoon/1193.cpp
@@ -3,17 +3,30 @@
 int main(){
     int x;
     int temp =0;
+    int whatLine =0;
     int result=0;
+    int denom=0;//분모
+    int numer=0;//분자 
     scanf("%d", &x);
     for(int i =1 ; i<x; i++){
-        if(i>=x){
+        if(i>=temp){
             result =i;
             break;
         }
-        temp = x-i;
-        
+        temp = x-i;// i번쨰 줄의 몇번쨰인지 
+        whatLine = i;//몇번째 줄인지
     }
-    printf("%d/%d", )
+    if(whatLine%2==0){//짝수번쨰 줄이라면
+    
+        numer = temp;
+        denom = whatLine-temp+1;
+      
+    }
+    else{
+        numer = temp;
+        denom = whatLine-temp+1;
+    }
+//    printf("%d/%d", )
 
 
     return 0;
