218ba8b7adfe68c8a6a5d88a203c93f9ea0a5f07
이명학 <leemh9207@ajou.ac.kr>
2020-07-02 23:11:06 +0900

    중간커밋

--- BaekJoon/1064.cpp
+++ BaekJoon/1064.cpp
@@ -9,30 +9,37 @@ using namespace std;
 typedef long double ld;
 
 int main (){
-    int Xarr[3];
-    int Yarr[3];
-    int length[3];
-    int length1, length2, length3;
-    int min= 1000000;
-    int max = 0;
-    int tempLength;
+    ld Xarr[3];
+    ld Yarr[3];
+    ld length[3];
+    ld length1, length2, length3;
+    ld min= 1000000;
+    ld max = 0;
+    ld tempLength;
     for (int i =0; i <3; i++){
-        scanf("%d", &Xarr[i]);
-        scanf("%d", &Yarr[i]);
+        scanf("%ld", &Xarr[i]);
+        scanf("%ld", &Yarr[i]);
     }
     length[0] = sqrt(pow((Xarr[0]-Xarr[1]),2)-pow((Yarr[0]-Yarr[1]),2));
     length[1] = sqrt(pow((Xarr[1]-Xarr[2]),2)-pow((Yarr[1]-Yarr[2]),2));
     length[2] = sqrt(pow((Xarr[0]-Xarr[2]),2)-pow((Yarr[0]-Yarr[2]),2));
-    int i =0;
+    if((Xarr[0]==Xarr[1]==Xarr[2])||(Yarr[0]==Yarr[1]==Yarr[2])){
+        printf("-1\n");
+    }
+    if()
+    int idx = 0;
     while(true){
         
-        tempLength = 2*length[i] + 2*length[++i];
+        tempLength = 2*length[idx] + 2*length[++idx];
         min = min(min, tempLength);
         max = max(max,tempLength);
-        printf("%d", i);
-        if( i==3) break;
+        printf("%d", idx);
+        
+        if( idx==3) break;
+
+
     }
-    
+    printf("%ld", max-min);
 
     
 
