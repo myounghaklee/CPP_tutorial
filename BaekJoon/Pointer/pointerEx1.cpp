665e5e20d43858d683cbdbd9353e54c74dedc554
이명학 <leemh9207@ajou.ac.kr>
2020-07-03 18:13:26 +0900

    포인터 정리중

---
+++ BaekJoon/Pointer/pointerEx1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int a;
+    int *p;
+    p = &a;
+    printf("%d\n",p);
+    a= 5;
+    printf("%d\n",p);// p가 가리키고 있는 a의 주소가 출력된다. 
+    printf("%d\n",&a); // a의 주소 출력된다 p의 출력 결과와 동일 
+    printf("%d\n",&p); // p의 주소 출력. 즉, p의주소에 p의 값인 a의 주소가 들어있다.
+    printf("%d\n",*p); // p가 가리키고 있는 a의 값인 5가 출력된다
+
+    return 0;
+}
