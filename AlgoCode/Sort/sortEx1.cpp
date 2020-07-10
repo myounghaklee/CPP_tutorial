4916e22af05e766c70e66cd8229993c79754d7b3
이명학 <leemh9207@ajou.ac.kr>
2020-07-10 16:33:02 +0900

    소트 추가

--- AlgoCode/Sort/sortEx1.cpp
+++ AlgoCode/Sort/sortEx1.cpp
@@ -27,4 +27,4 @@ int main(){
     for( int i=0; i<10;i++){
         printf("%d\n",a[i]);
     }
-} 
+}  
---
+++ AlgoCode/Sort/sortEx2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//class: 여러개의 변수를 하나로 묶기 위해서 사용
+class Student{
+public :
+    string name;
+    int score;
+    Student(string name, int score){ //생성자 
+        this -> name = name;
+        this -> score = score;
+    }
+    //정렬기준 = 점수가 작은 순서로 
+    bool operator <(Student & student){
+        return this ->score <student.score;
+    }
+};
+bool compare ( int a, int b){
+    return a>b;       
+}
+
+int main (){
+    Student students[] = {
+        Student("나동빈",90),
+        Student("이상욱",93),
+        Student("박한울",97),
+        Student("강종구",87),
+        Student("이태일",92)
+    };
+    sort(students, students+5);
+    for( int i=0; i<5;i++){
+        cout <<students[i].name << ' ';
+    }
+}
